37c777cf4348565e1dfa46ee01898fa3588a0628
vladdruzjakin <drz.vladd@gmail.com>
2020-05-18 23:01:23 +0300

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import { Timestamp } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  styleUrls: [ './app.component.scss' ],
+    animations: [
+    trigger('slowAnimate', [
+      transition(':enter', [style({opacity: '0'}), animate(1000)]),
+      transition(':leave', [style({opacity: '1'}), animate(1000, style({opacity: '0'}))]),
+    ])
+  ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  public stateAnimate: boolean;
+
+  constructor() {
+    this.stateAnimate = false;
+  }
+
+  public animate(): void {
+    this.stateAnimate = true;
+    setTimeout(() => {
+      return this.stateAnimate = false;
+    }, 2000);
+  }
 }
+
